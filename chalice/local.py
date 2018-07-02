60cadadd21abc3f96d60a0486083cb179f588991
ramit.surana <ramit.surana@reancloud.com>
2018-07-02 19:13:38 +0530

    $commit_message

---
+++ chalice/local.py
@@ -0,0 +1,249 @@
+"""Dev server used for running a chalice app locally.
+
+This is intended only for local development purposes.
+
+"""
+from __future__ import print_function
+import base64
+import functools
+from collections import namedtuple
+
+from six.moves.BaseHTTPServer import HTTPServer
+from six.moves.BaseHTTPServer import BaseHTTPRequestHandler
+from typing import List, Any, Dict, Tuple, Callable  # noqa
+
+from chalice.app import Chalice, CORSConfig  # noqa
+from chalice.compat import urlparse, parse_qs
+
+
+MatchResult = namedtuple('MatchResult', ['route', 'captured', 'query_params'])
+EventType = Dict[str, Any]
+HandlerCls = Callable[..., 'ChaliceRequestHandler']
+ServerCls = Callable[..., 'HTTPServer']
+
+
+def create_local_server(app_obj, port):
+    # type: (Chalice, int) -> LocalDevServer
+    return LocalDevServer(app_obj, port)
+
+
+class RouteMatcher(object):
+    def __init__(self, route_urls):
+        # type: (List[str]) -> None
+        # Sorting the route_urls ensures we always check
+        # the concrete routes for a prefix before the
+        # variable/capture parts of the route, e.g
+        # '/foo/bar' before '/foo/{capture}'
+        self.route_urls = sorted(route_urls)
+
+    def match_route(self, url):
+        # type: (str) -> MatchResult
+        """Match the url against known routes.
+
+        This method takes a concrete route "/foo/bar", and
+        matches it against a set of routes.  These routes can
+        use param substitution corresponding to API gateway patterns.
+        For example::
+
+            match_route('/foo/bar') -> '/foo/{name}'
+
+        """
+        # Otherwise we need to check for param substitution
+        parsed_url = urlparse(url)
+        query_params = {k: v[0] for k, v in parse_qs(parsed_url.query).items()}
+        parts = parsed_url.path.split('/')
+        captured = {}
+        for route_url in self.route_urls:
+            url_parts = route_url.split('/')
+            if len(parts) == len(url_parts):
+                for i, j in zip(parts, url_parts):
+                    if j.startswith('{') and j.endswith('}'):
+                        captured[j[1:-1]] = i
+                        continue
+                    if i != j:
+                        break
+                else:
+                    return MatchResult(route_url, captured, query_params)
+        raise ValueError("No matching route found for: %s" % url)
+
+
+class LambdaEventConverter(object):
+
+    LOCAL_SOURCE_IP = '127.0.0.1'
+
+    """Convert an HTTP request to an event dict used by lambda."""
+    def __init__(self, route_matcher, binary_types=None):
+        # type: (RouteMatcher, List[str]) -> None
+        self._route_matcher = route_matcher
+        if binary_types is None:
+            binary_types = []
+        self._binary_types = binary_types
+
+    def _is_binary(self, headers):
+        # type: (Dict[str,Any]) -> bool
+        return headers.get('content-type', '') in self._binary_types
+
+    def create_lambda_event(self, method, path, headers, body=None):
+        # type: (str, str, Dict[str, str], str) -> EventType
+        view_route = self._route_matcher.match_route(path)
+        event = {
+            'requestContext': {
+                'httpMethod': method,
+                'resourcePath': view_route.route,
+                'identity': {
+                    'sourceIp': self.LOCAL_SOURCE_IP
+                },
+            },
+            'headers': dict(headers),
+            'queryStringParameters': view_route.query_params,
+            'pathParameters': view_route.captured,
+            'stageVariables': {},
+        }
+        if body is None:
+            event['body'] = '{}'
+        elif self._is_binary(headers):
+            event['body'] = base64.b64encode(body).decode('ascii')
+            event['isBase64Encoded'] = True
+        else:
+            event['body'] = body
+        return event
+
+
+class ChaliceRequestHandler(BaseHTTPRequestHandler):
+
+    protocol = 'HTTP/1.1'
+
+    def __init__(self, request, client_address, server, app_object):
+        # type: (bytes, Tuple[str, int], HTTPServer, Chalice) -> None
+        self.app_object = app_object
+        self.event_converter = LambdaEventConverter(
+            RouteMatcher(list(app_object.routes)),
+            self.app_object.api.binary_types
+        )
+        BaseHTTPRequestHandler.__init__(
+            self, request, client_address, server)  # type: ignore
+        # Force BaseHTTPRequestHandler to use HTTP/1.1
+        # Chrome ignores many headers from HTTP/1.0 servers.
+        BaseHTTPRequestHandler.protocol_version = "HTTP/1.1"
+
+    def _generic_handle(self):
+        # type: () -> None
+        lambda_event = self._generate_lambda_event()
+        self._do_invoke_view_function(lambda_event)
+
+    def _handle_binary(self, response):
+        # type: (Dict[str,Any]) -> Dict[str,Any]
+        if response.get('isBase64Encoded'):
+            body = base64.b64decode(response['body'])
+            response['body'] = body
+        return response
+
+    def _do_invoke_view_function(self, lambda_event):
+        # type: (EventType) -> None
+        lambda_context = None
+        response = self.app_object(lambda_event, lambda_context)
+        response = self._handle_binary(response)
+        self._send_http_response(lambda_event, response)
+
+    def _send_http_response(self, lambda_event, response):
+        # type: (EventType, Dict[str, Any]) -> None
+        self.send_response(response['statusCode'])
+        self.send_header('Content-Length', str(len(response['body'])))
+        content_type = response['headers'].pop(
+            'Content-Type', 'application/json')
+        self.send_header('Content-Type', content_type)
+        headers = response['headers']
+        for header in headers:
+            self.send_header(header, headers[header])
+        self.end_headers()
+        body = response['body']
+        if not isinstance(body, bytes):
+            body = body.encode('utf-8')
+        self.wfile.write(body)
+
+    def _generate_lambda_event(self):
+        # type: () -> EventType
+        content_length = int(self.headers.get('content-length', '0'))
+        body = None
+        if content_length > 0:
+            body = self.rfile.read(content_length)
+        # mypy doesn't like dict(self.headers) so I had to use a
+        # dictcomp instead to make it happy.
+        converted_headers = {key: value for key, value in self.headers.items()}
+        lambda_event = self.event_converter.create_lambda_event(
+            method=self.command, path=self.path, headers=converted_headers,
+            body=body,
+        )
+        return lambda_event
+
+    do_GET = do_PUT = do_POST = do_HEAD = do_DELETE = do_PATCH = \
+        _generic_handle
+
+    def do_OPTIONS(self):
+        # type: () -> None
+        # This can either be because the user's provided an OPTIONS method
+        # *or* this is a preflight request, which chalice automatically
+        # sets up for you.
+        lambda_event = self._generate_lambda_event()
+        if self._has_user_defined_options_method(lambda_event):
+            self._do_invoke_view_function(lambda_event)
+        else:
+            # Otherwise this is a preflight request which we automatically
+            # generate.
+            self._send_autogen_options_response(lambda_event)
+
+    def _has_user_defined_options_method(self, lambda_event):
+        # type: (EventType) -> bool
+        route_key = lambda_event['requestContext']['resourcePath']
+        return 'OPTIONS' in self.app_object.routes[route_key]
+
+    def _send_autogen_options_response(self, lambda_event):
+        # type:(EventType) -> None
+        route_key = lambda_event['requestContext']['resourcePath']
+        route_dict = self.app_object.routes[route_key]
+        route_methods = list(route_dict.keys())
+
+        # Chalice ensures that routes with multiple views have the same
+        # CORS configuration, so if any view has a CORS Config we can use
+        # that config since they will all be the same.
+        cors_config = route_dict[route_methods[0]].cors
+        cors_headers = cors_config.get_access_control_headers()
+
+        # We need to add OPTIONS since it is not a part of the CORSConfig
+        # object. APIGateway handles this entirely based on the API definition.
+        # So our local version needs to add this manually to our set of allowed
+        # headers.
+        route_methods.append('OPTIONS')
+        route_methods = route_methods
+
+        # The Access-Control-Allow-Methods header is not added by the
+        # CORSConfig object it is added to the API Gateway route during
+        # deployment, so we need to manually add those headers here.
+        cors_headers.update({
+            'Access-Control-Allow-Methods': '%s' % ','.join(route_methods)
+        })
+
+        self.send_response(200)
+        for k, v in cors_headers.items():
+            self.send_header(k, v)
+        self.end_headers()
+
+
+class LocalDevServer(object):
+    def __init__(self, app_object, port, handler_cls=ChaliceRequestHandler,
+                 server_cls=HTTPServer):
+        # type: (Chalice, int, HandlerCls, ServerCls) -> None
+        self.app_object = app_object
+        self.port = port
+        self._wrapped_handler = functools.partial(
+            handler_cls, app_object=app_object)
+        self.server = server_cls(('', port), self._wrapped_handler)
+
+    def handle_single_request(self):
+        # type: () -> None
+        self.server.handle_request()
+
+    def serve_forever(self):
+        # type: () -> None
+        print("Serving on localhost:%s" % self.port)
+        self.server.serve_forever()
---
+++ chalice/utils.py
@@ -0,0 +1,185 @@
+import os
+import zipfile
+import json
+import contextlib
+import tempfile
+import shutil
+import tarfile
+
+from typing import IO, Dict, List, Any, Tuple, Iterator, BinaryIO  # noqa
+
+from chalice.constants import WELCOME_PROMPT
+
+
+def remove_stage_from_deployed_values(key, filename):
+    # type: (str, str) -> None
+    """Delete a top level key from the deployed JSON file."""
+    final_values = {}  # type: Dict[str, Any]
+    try:
+        with open(filename, 'r') as f:
+            final_values = json.load(f)
+    except IOError:
+        # If there is no file to delete from, then this funciton is a noop.
+        return
+
+    try:
+        del final_values[key]
+        with open(filename, 'wb') as f:
+            data = json.dumps(final_values, indent=2, separators=(',', ': '))
+            f.write(data.encode('utf-8'))
+    except KeyError:
+        # If they key didn't exist then there is nothing to remove.
+        pass
+
+
+def record_deployed_values(deployed_values, filename):
+    # type: (Dict[str, str], str) -> None
+    """Record deployed values to a JSON file.
+
+    This allows subsequent deploys to lookup previously deployed values.
+
+    """
+    final_values = {}  # type: Dict[str, Any]
+    if os.path.isfile(filename):
+        with open(filename, 'r') as f:
+            final_values = json.load(f)
+    final_values.update(deployed_values)
+    with open(filename, 'wb') as f:
+        data = json.dumps(final_values, indent=2, separators=(',', ': '))
+        f.write(data.encode('utf-8'))
+
+
+def create_zip_file(source_dir, outfile):
+    # type: (str, str) -> None
+    """Create a zip file from a source input directory.
+
+    This function is intended to be an equivalent to
+    `zip -r`.  You give it a source directory, `source_dir`,
+    and it will recursively zip up the files into a zipfile
+    specified by the `outfile` argument.
+
+    """
+    with zipfile.ZipFile(outfile, 'w',
+                         compression=zipfile.ZIP_DEFLATED) as z:
+        for root, _, filenames in os.walk(source_dir):
+            for filename in filenames:
+                full_name = os.path.join(root, filename)
+                archive_name = os.path.relpath(full_name, source_dir)
+                z.write(full_name, archive_name)
+
+
+class OSUtils(object):
+    ZIP_DEFLATED = zipfile.ZIP_DEFLATED
+
+    def open(self, filename, mode):
+        # type: (str, str) -> IO
+        return open(filename, mode)
+
+    def open_zip(self, filename, mode, compression=ZIP_DEFLATED):
+        # type: (str, str, int) -> zipfile.ZipFile
+        return zipfile.ZipFile(filename, mode, compression=compression)
+
+    def remove_file(self, filename):
+        # type: (str) -> None
+        """Remove a file, noop if file does not exist."""
+        # Unlike os.remove, if the file does not exist,
+        # then this method does nothing.
+        try:
+            os.remove(filename)
+        except OSError:
+            pass
+
+    def file_exists(self, filename):
+        # type: (str) -> bool
+        return os.path.isfile(filename)
+
+    def get_file_contents(self, filename, binary=True):
+        # type: (str, bool) -> str
+        if binary:
+            mode = 'rb'
+        else:
+            mode = 'r'
+        with open(filename, mode) as f:
+            return f.read()
+
+    def set_file_contents(self, filename, contents, binary=True):
+        # type: (str, str, bool) -> None
+        if binary:
+            mode = 'wb'
+        else:
+            mode = 'w'
+        with open(filename, mode) as f:
+            f.write(contents)
+
+    def extract_zipfile(self, zipfile_path, unpack_dir):
+        # type: (str, str) -> None
+        with zipfile.ZipFile(zipfile_path, 'r') as z:
+            z.extractall(unpack_dir)
+
+    def extract_tarfile(self, tarfile_path, unpack_dir):
+        # type: (str, str) -> None
+        with tarfile.open(tarfile_path, 'r:gz') as tar:
+            tar.extractall(unpack_dir)
+
+    def directory_exists(self, path):
+        # type: (str) -> bool
+        return os.path.isdir(path)
+
+    def get_directory_contents(self, path):
+        # type: (str) -> List[str]
+        return os.listdir(path)
+
+    def makedirs(self, path):
+        # type: (str) -> None
+        os.makedirs(path)
+
+    def dirname(self, path):
+        # type: (str) -> str
+        return os.path.dirname(path)
+
+    def abspath(self, path):
+        # type: (str) -> str
+        return os.path.abspath(path)
+
+    def joinpath(self, *args):
+        # type: (str) -> str
+        return os.path.join(*args)
+
+    def walk(self, path):
+        # type: (str) -> Iterator[Tuple[str, List[str], List[str]]]
+        return os.walk(path)
+
+    def copytree(self, source, destination):
+        # type: (str, str) -> None
+        if not os.path.exists(destination):
+            self.makedirs(destination)
+        names = self.get_directory_contents(source)
+        for name in names:
+            new_source = os.path.join(source, name)
+            new_destination = os.path.join(destination, name)
+            if os.path.isdir(new_source):
+                self.copytree(new_source, new_destination)
+            else:
+                shutil.copy2(new_source, new_destination)
+
+    def rmtree(self, directory):
+        # type: (str) -> None
+        shutil.rmtree(directory)
+
+    def move(self, source, destination):
+        # type: (str, str) -> None
+        shutil.move(source, destination)
+
+    @contextlib.contextmanager
+    def tempdir(self):
+        # type: () -> Any
+        tempdir = tempfile.mkdtemp()
+        try:
+            yield tempdir
+        finally:
+            shutil.rmtree(tempdir)
+
+
+def getting_started_prompt(prompter):
+    # type: (Any) -> bool
+    return prompter.prompt(WELCOME_PROMPT)
